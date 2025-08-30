79fc010741a9a80af3f11cbcdca38888c05fd033
Eduardo Dias <eduardodias@gmail.com>
2025-08-30 14:16:12 +1000

    Renames SourceSelection references to RestClient package
    - Renames the package from RestClient to SourceSelection
    - Updates the target names to reflect the new package name
    - Adds iOS and macOS platform support
    - Adds RestClient as a package dependency

--- Packages/Features/SourceSeletion/Package.swift
+++ Packages/Features/SourceSeletion/Package.swift
@@ -4,23 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "RestClient",
+    name: "SourceSelection",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "RestClient",
-            targets: ["RestClient"]
-        )
+            name: "SourceSelection",
+            targets: ["SourceSelection"]),
+    ],
+    dependencies: [
+        .package(path: "../../Network/RestClient")
+
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RestClient"
-        ),
+            name: "SourceSelection",
+            dependencies: [
+                .product(name: "RestClient", package: "RestClient")
+            ]),
         .testTarget(
-            name: "RestClientTests",
-            dependencies: ["RestClient"]
-        )
-    ]
+            name: "SourceSelectionTests",
+            dependencies: ["SourceSelection"]
+        ),
+    ],
 )
