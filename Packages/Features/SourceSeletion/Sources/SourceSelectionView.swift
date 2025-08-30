f9d86a14e8bbfbec180bc17b141c52b86f3bbe61
Eduardo Dias <eduardodias@gmail.com>
2025-08-30 14:20:00 +1000

    Adds source selection view
    Implements a basic view for selecting a news source

---
+++ Packages/Features/SourceSeletion/Sources/SourceSelectionView.swift
@@ -0,0 +1,15 @@
+//
+//  SourceSelectionView.swift
+//  SourceSelection
+//
+//  Created by Eduardo Dias on 30/08/2025.
+//
+
+import SwiftUI
+
+struct SourceSelectionView: View {
+
+    var body: some View {
+        Text("Hello")
+    }
+}
