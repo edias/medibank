b354e908b204928d0b00cbb41e60b0aaa7467d59
Eduardo Dias <eduardodias@gmail.com>
2025-09-01 08:12:32 +1000

    Remove commented-out Article and Source models from Headlines

--- Packages/Features/Headlines/Sources/Network/ArticlesResponse.swift
+++ Packages/Features/Headlines/Sources/Network/ArticlesResponse.swift
@@ -11,29 +11,3 @@ import Storage
 struct ArticlesResponse: Decodable {
     let articles: [Article]
 }
-
-//struct Article: Codable, Identifiable {
-//    let id: UUID = .init()
-//    let source: Source
-//    let author: String?
-//    let title: String
-//    let description: String
-//    let url: String
-//    let urlToImage: String
-//    let publishedAt: String
-//}
-//
-//extension Article {
-//    var authorName: String {
-//        author ?? source.name
-//    }
-//
-//    var sourceName: String {
-//        source.name
-//    }
-//}
-//
-//struct Source: Codable {
-//    let id: String
-//    let name: String
-//}
