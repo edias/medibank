420d4030b8f4e7613b17a3580ffa5ef3194eab1e
Eduardo Dias <eduardodias@gmail.com>
2025-09-01 13:45:23 +1000

    Implements EmptyStatView on CommonUI

---
+++ Packages/Share/CommonUI/Sources/Views/EmptyStateView.swift
@@ -0,0 +1,50 @@
+//
+//  EmptyStateView.swift
+//  CommonUI
+//
+//  Created by Eduardo Dias on 01/09/2025.
+//
+
+import SwiftUI
+
+public struct EmptyStateView: View {
+
+    let emptyState: EmptyState
+
+    public init(emptyState: EmptyState) {
+        self.emptyState = emptyState
+    }
+
+    public var body: some View {
+
+        VStack(spacing: DesignSystem.metrics.extraLarge) {
+
+            Image(systemName: emptyState.iconName)
+                .font(.system(size: DesignSystem.metrics.buttonSize))
+                .foregroundColor(DesignSystem.colors.secondaryText)
+
+            Text(emptyState.title)
+                .font(DesignSystem.typography.title2)
+                .fontWeight(DesignSystem.typography.semibold)
+
+            Text(emptyState.description)
+                .font(DesignSystem.typography.body)
+                .foregroundColor(DesignSystem.colors.secondaryText)
+                .multilineTextAlignment(.center)
+        }
+        .padding(DesignSystem.metrics.massive)
+    }
+}
+
+public struct EmptyState {
+
+    let iconName: String
+    let title: String
+    let description: String
+
+    public init(iconName: String, title: String, description: String) {
+        self.iconName = iconName
+        self.title = title
+        self.description = description
+    }
+}
